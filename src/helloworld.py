48069913c3b7f3d7ba1833b8f221ba2df45c0cb4
Chris Smith <chris@dmdirc.com>
2010-06-30 14:35:06 +0100

    Add showing/adding accounts

--- src/helloworld.py
+++ src/helloworld.py
@@ -5,16 +5,12 @@ from google.appengine.api import users
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class User(db.Model):
-    user = db.UserProperty()
-    joindate = db.DateTimeProperty(auto_now_add=True)
-
 class AchievementSource(db.Model):
     name = db.StringProperty()
     url = db.LinkProperty()
 
 class UserAccount(db.Model):
-    user = db.ReferenceProperty(reference_class=User)
+    user = db.UserProperty()
     source = db.ReferenceProperty(reference_class=AchievementSource)
     credentials = db.StringProperty()
     added = db.DateTimeProperty(auto_now_add=True)
@@ -28,7 +24,7 @@ class Achievement(db.Model):
 
 class AwardedAchievement(db.Model):
     achievement = db.ReferenceProperty(reference_class=Achievement)
-    user = db.ReferenceProperty(reference_class=User)
+    user = db.UserProperty()
     awarded = db.DateTimeProperty()
     discovered = db.DateTimeProperty(auto_now_add=True)
 
@@ -44,6 +40,8 @@ class MainPage(webapp.RequestHandler):
 
         if users.is_current_user_admin():
             self.show_admin_form()
+
+        self.show_sources()
 
         self.show_footer()
 
@@ -62,7 +60,7 @@ class MainPage(webapp.RequestHandler):
 
     def show_admin_form(self):
         self.response.out.write("""
-          <h1>Sources</h1>
+          <h1>Admin - Sources</h1>
           <h2>Add</h2>
           <form action="/admin/addsource" method="post">
            <label>Name: <input type="text" name="name"/></label>
@@ -75,11 +73,42 @@ class MainPage(webapp.RequestHandler):
            """)
 
         for source in AchievementSource.all():
-            self.response.out.write("<tr><td>");
+            self.response.out.write("<tr><td>")
             self.response.out.write(cgi.escape(source.name))
-            self.response.out.write("</td><td>");
+            self.response.out.write("</td><td>")
             self.response.out.write(cgi.escape(source.url))
-            self.response.out.write("</td></tr>");
+            self.response.out.write("</td></tr>")
+
+        self.response.out.write("</table>")
+
+    def show_sources(self):
+        self.response.out.write("<h1>My Accounts</h1>")
+        self.response.out.write("<table>")
+        self.response.out.write("<tr><th>Source</th><th>Credentials</th></tr>")
+
+        for account in UserAccount.gql("WHERE user = :user", user=users.get_current_user()):
+            self.response.out.write("<tr><td>")
+            self.response.out.write(cgi.escape(account.source.name))
+            self.response.out.write("</td><td>")
+            self.response.out.write(cgi.escape(account.credentials))
+            self.response.out.write("</td></tr>")
+
+        self.response.out.write("</table>")
+        self.response.out.write("""<h2>Add Account</h2>
+          <form action="/addaccount" method="post">
+           <label>Type: <select name="type">""")
+
+        for source in AchievementSource.all():
+            self.response.out.write('<option value="')
+            self.response.out.write(source.key())
+            self.response.out.write('">')
+            self.response.out.write(cgi.escape(source.name))
+            self.response.out.write('</option>')
+
+        self.response.out.write("""</select></label>
+           <label>Credentials: <input type="text" name="credentials"/></label>
+           <input type="submit" value="Add"/>
+          </form>""")
 
 class AddSourcePage(webapp.RequestHandler):
     def post(self):
@@ -94,16 +123,31 @@ class AddSourcePage(webapp.RequestHandler):
 
         self.redirect('/')
 
-
-class Guestbook(webapp.RequestHandler):
+class AddAccountPage(webapp.RequestHandler):
     def post(self):
-        self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('content')))
-        self.response.out.write('</pre></body></html>')
+
+        if not users.get_current_user():
+            self.error(403)
+            return
+
+        source = db.get(db.Key(self.request.get('type')))
+
+        account = UserAccount(user=users.get_current_user(),
+                              source=source,
+                              credentials=self.request.get('credentials'))
+        account.put()
+
+        self.redirect('/')
+
+
+class UpdatePage(webapp.RequestHandler):
+    def post(self):
+        account = db.get(db.Key(self.request.get('key')))
 
 application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/sign', Guestbook),
-                                      ('/admin/addsource', AddSourcePage)],
+                                      ('/admin/addsource', AddSourcePage),
+                                      ('/worker/update', UpdatePage),
+                                      ('/addaccount', AddAccountPage)],
                                      debug=True)
 
 def main():
