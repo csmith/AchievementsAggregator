1bf9bef659124839abc8cff72e0d988bb7e1ac75
Chris Smith <chris@dmdirc.com>
2010-07-01 13:52:17 +0100

    Scraper for steam games page

--- src/Scraper.py
+++ src/Scraper.py
@@ -24,3 +24,24 @@ class Scraper:
           handleError(e)
 
         return results
+
+    @staticmethod
+    def scrape_steam(credentials):
+        results = []
+        prefix = "http://steamcommunity.com/id/%s/"
+        url = "%sgames?xml=1" % (prefix % credentials)
+
+        try:
+          result = urllib2.urlopen(url).read()
+          soup = BeautifulSoup(result)
+
+          for globalLink in soup.findAll('globalstatslink'):
+            game = globalLink.parent
+            name = game.find('name').string.strip()
+            url = prefix + game.find('statslink').string.strip()[len(prefix % credentials):]
+            results.append({'name': name, 'url': url})
+
+        except urllib2.URLError, e:
+          handleError(e)
+
+        return results
